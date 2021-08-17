fac332aa47dbc906ca9cdb9c10347e6af870127c
Ferdi Gökdemir <ferdi.gokdemir@akinon.com>
2021-08-17 21:55:05 +0300

    general

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 4242;
 
 (async () => {
   try {
-    mongoose.connect( process.env.MONGO_URI || "", { useNewUrlParser: true });
+    mongoose.connect( process.env.DATABASE_URI || "", { useNewUrlParser: true });
     const app = express();
 
     app.use(helmet());
